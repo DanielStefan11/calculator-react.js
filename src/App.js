7b3c5559c8b3bfb91e729427b93f24cae947e55d
DanielStefan11 <dannielstefan592@gmail.com>
2021-03-06 13:00:18 +0200

    Improved styling on mobile devices

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
-import React from "react";
+import React from 'react';
 // Components
-import Output from "./components/output/Output";
-import Button from "./components/button/Button";
-import ResetButton from "./components/resetButton/ResetButton";
+import Output from './components/output/Output';
+import Button from './components/button/Button';
+import ResetButton from './components/resetButton/ResetButton';
 // CSS
-import "./App.css";
+import './App.css';
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: "",
+      input: '',
     };
   }
 
@@ -19,9 +19,9 @@ class App extends React.Component {
   };
 
   backspace = () => {
-    if (typeof this.state.input === "string") {
+    if (typeof this.state.input === 'string') {
       this.setState({ input: this.state.input.slice(0, -1) });
-    } else if (typeof this.state.input === "number") {
+    } else if (typeof this.state.input === 'number') {
       this.setState({ input: this.state.input.toString().slice(0, -1) });
     }
   };
@@ -31,60 +31,60 @@ class App extends React.Component {
       this.setState({
         input:
           eval(this.state.input) === Infinity
-            ? "Can not be divided by 0"
+            ? 'Can not be divided by 0'
             : eval(this.state.input),
       });
     } catch (e) {
-      this.setState({ input: "Error - click Reset" });
+      this.setState({ input: 'Error - click Reset' });
     }
   };
 
   reset = () => {
-    this.setState({ input: "" });
+    this.setState({ input: '' });
   };
 
   render() {
     return (
-      <div className="App">
-        <h1 className="calc-heading">React Calculator</h1>
-        <div className="calc-container">
-          <div className="row">
+      <div className='App'>
+        <h1 className='calc-heading'>React Calculator</h1>
+        <div className='calc-container'>
+          <div className='row'>
             <Output>{this.state.input}</Output>
           </div>
 
-          <div className="row">
+          <div className='row'>
             <Button handleClick={this.addToInput}>(</Button>
             <Button handleClick={this.backspace}>Backspace</Button>
             <Button handleClick={this.addToInput}>)</Button>
           </div>
-          <div className="row">
+          <div className='row'>
             <Button handleClick={this.addToInput}>7</Button>
             <Button handleClick={this.addToInput}>8</Button>
             <Button handleClick={this.addToInput}>9</Button>
             <Button handleClick={this.addToInput}>/</Button>
           </div>
 
-          <div className="row">
+          <div className='row'>
             <Button handleClick={this.addToInput}>4</Button>
             <Button handleClick={this.addToInput}>5</Button>
             <Button handleClick={this.addToInput}>6</Button>
             <Button handleClick={this.addToInput}>*</Button>
           </div>
 
-          <div className="row">
+          <div className='row'>
             <Button handleClick={this.addToInput}>1</Button>
             <Button handleClick={this.addToInput}>2</Button>
             <Button handleClick={this.addToInput}>3</Button>
             <Button handleClick={this.addToInput}>+</Button>
           </div>
 
-          <div className="row">
+          <div className='row'>
             <Button handleClick={this.addToInput}>.</Button>
             <Button handleClick={this.addToInput}>0</Button>
             <Button handleClick={this.calculate}>=</Button>
             <Button handleClick={this.addToInput}>-</Button>
           </div>
-          <div className="row">
+          <div className='row'>
             <ResetButton handleReset={this.reset}>Reset</ResetButton>
           </div>
         </div>
--- src/components/button/Button.jsx
+++ src/components/button/Button.jsx
@@ -1,15 +1,17 @@
-import React from "react";
+import React from 'react';
 // CSS
-import "./Button.css";
+import './Button.css';
 
 export default function Button(props) {
   function isOperator(value) {
-    return !isNaN(value) || (value === "." && value === "=");
+    return !isNaN(value) || (value === '.' && value === '=');
   }
 
   return (
     <div
-      className={`button ${isOperator(props.children) ? "" : "operator"}`}
+      className={`button ${isOperator(props.children) ? '' : 'operator'} ${
+        props.children === 'Backspace' && 'backspace-btn'
+      }`}
       onClick={() => props.handleClick(props.children)}
     >
       {props.children}
